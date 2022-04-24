a8fb36e5f5d067f804201bb8d03a36c0cfc21881
Isa Gutierrez <isagr_kio@hotmail.com>
2022-04-24 11:55:18 +0200

    heroku

--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-    baseURL: 'http://localhost:3001/api',
+    baseURL: `${process.env.REACT_APP_API_HOST}/api` || 'http://localhost:3001/api',
     withCredentials: false
 })
 
